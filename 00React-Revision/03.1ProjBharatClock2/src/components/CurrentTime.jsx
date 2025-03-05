43067e4f375f860610ca429ebd4234a8b63fce8d
kk0707singh <kk0707singh@gmail.com>
2025-03-05 19:21:16 +0530

    bharat clock optimised

---
+++ 00React-Revision/03.1ProjBharatClock2/src/components/CurrentTime.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+function CurrentTime() {
+    const [time, setTime] = useState(new Date());
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return() => {
+            clearInterval(intervalId);
+        }
+    }, [])
+    return (
+        <p>This is the current Time: {time.toLocaleDateString()}-{time.toLocaleTimeString()}</p>
+    )
+}
+
+export default CurrentTime
